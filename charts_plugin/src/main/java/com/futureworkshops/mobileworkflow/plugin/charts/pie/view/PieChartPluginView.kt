e7b369738abb28d24b67871dae604392678d1313
Manuel Perera <mapm14@gmail.com>
2020-10-30 11:33:23 +0100

    Apply styling to Pie Chart and fix bugs

--- charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/pie/view/PieChartPluginView.kt
+++ charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/pie/view/PieChartPluginView.kt
@@ -5,9 +5,11 @@
 package com.futureworkshops.mobileworkflow.plugin.charts.pie.view
 
 import android.content.Context
+import android.graphics.Color
 import com.futureworkshops.mobileworkflow.plugin.charts.R
 import com.futureworkshops.mobileworkflow.plugin.charts.pie.step.PieChartItem
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
@@ -22,7 +24,7 @@ internal class PieChartPluginView(
     isOptional: Boolean,
     title: String,
     val items: List<PieChartItem>
-) : QuestionView(context, id, isOptional, title, null, "Next!") {
+) : QuestionView(context, id, isOptional, title, null, "Next") {
 
     private lateinit var pieChartPluginPart: PieChartPluginPart
 
@@ -39,9 +41,25 @@ internal class PieChartPluginView(
             PieEntry(item.value, item.label)
         }
 
-        val pieDataSet = PieDataSet(map, "Manu Pie Chart")
+        val pieDataSet = PieDataSet(map, "")
+        pieDataSet.setColors(
+            intArrayOf(
+                R.color.chart_color_1,
+                R.color.chart_color_2,
+                R.color.chart_color_3,
+                R.color.chart_color_4,
+                R.color.chart_color_5
+            ), context
+        )
+        pieDataSet.valueTextColor = Color.BLACK
         val pieData = PieData(pieDataSet)
-        pieChartPluginPart.view.findViewById<PieChart>(R.id.chart).data = pieData
+        val pieChart = pieChartPluginPart.view.findViewById<PieChart>(R.id.pieChart)
+        pieChart.data = pieData
+        pieChart.setEntryLabelColor(Color.BLACK)
+        pieChart.description = Description().apply {
+            text = context.getString(R.string.quantities)
+        }
+        pieChart.invalidate()
     }
 
 }
