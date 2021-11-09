3b0ebc1e413947d61828a754cc280fffe51c03d1
Igor Castañeda Ferreira <igorcferreira@hotmail.com>
2021-11-09 19:38:55 +0100

    Finish bump to gradle 7.0.2

--- charts_plugin/build.gradle.kts
+++ charts_plugin/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 }
 
 android {
-    compileSdkVersion(30)
-    buildToolsVersion("30.0.3")
+    compileSdk = 31
+    buildToolsVersion = "31.0.0"
 
     defaultConfig {
-        minSdkVersion(24)
-        targetSdkVersion(30)
+        minSdk = 24
+        targetSdk = 31
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -104,8 +104,8 @@ dependencies {
     val fromMaven = project.property("project.mavenCore")?.toString()?.toBoolean() ?: false
     if (findProject(":mw-core") == null || fromMaven) {
         val version = project.property("project.coreVersion") as? String
-        implementation("com.futureworkshops.mobileworkflow:mw-core:$version")
+        api("com.futureworkshops.mobileworkflow:mw-core:$version")
     } else {
-        implementation(project(":mw-core"))
+        api(project(":mw-core"))
     }
 }
