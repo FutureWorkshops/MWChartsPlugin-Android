30d14236d80e0ed1ca34f94dc4534da49d6ed4ba
Igor Ferreira <igorcferreira@hotmail.com>
2022-01-10 12:02:09 -0300

    Fix usage of id propery

--- charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/pie/step/NetworkPieChartPluginStep.kt
+++ charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/pie/step/NetworkPieChartPluginStep.kt
@@ -2,10 +2,12 @@ package com.futureworkshops.mobileworkflow.plugin.charts.pie.step
 
 import android.os.Parcelable
 import com.futureworkshops.mobileworkflow.model.step.PluginStep
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class NetworkPieChartPluginStep(
+    override val id: String,
     override val title: String,
     override val type: String,
     val optional: Boolean,
--- charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/pie/step/PieChartPluginStep.kt
+++ charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/pie/step/PieChartPluginStep.kt
@@ -6,10 +6,12 @@ package com.futureworkshops.mobileworkflow.plugin.charts.pie.step
 
 import android.os.Parcelable
 import com.futureworkshops.mobileworkflow.model.step.PluginStep
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class PieChartPluginStep(
+    override val id: String,
     override val title: String,
     override val type: String,
     val optional: Boolean,
