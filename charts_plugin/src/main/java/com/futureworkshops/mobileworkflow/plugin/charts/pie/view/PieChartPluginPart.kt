8e2e075b55050311641e1614a5ef5595371f4609
Tomer Mandel <tomer@futureworkshops.com>
2022-05-02 10:23:18 +0200

    Remove use of StyleablePart and surveyTheme.

--- charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/pie/view/PieChartPluginPart.kt
+++ charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/pie/view/PieChartPluginPart.kt
@@ -8,21 +8,13 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
-import com.futureworkshops.mobileworkflow.SurveyTheme
-import com.futureworkshops.mobileworkflow.backend.views.main_parts.StyleablePart
 import com.futureworkshops.mobileworkflow.plugin.charts.R
 
 internal class PieChartPluginPart @JvmOverloads constructor(
     context: Context,
-    private val styleListener: StyleListener,
     attrs: AttributeSet? = null,
     defStyleRes: Int = 0
-) : FrameLayout(context, attrs, defStyleRes), StyleablePart {
+) : FrameLayout(context, attrs, defStyleRes) {
 
     val view: View = View.inflate(context, R.layout.pie_chart_plugin_step, this)
-
-    override fun style(surveyTheme: SurveyTheme) {
-        styleListener.onTintColorReady(surveyTheme.themeColor)
-    }
-
 }
