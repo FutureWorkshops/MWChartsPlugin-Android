d0082851ab4e4837579c4b49861a62b21b21db50
Igor Castañeda Ferreira <me@igorcferreira.dev>
2022-06-16 14:35:06 +0200

    Provide plugin version

--- charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/ChartsPluginFactory.kt
+++ charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/ChartsPluginFactory.kt
@@ -1,6 +1,7 @@
 package com.futureworkshops.mobileworkflow.plugin.charts
 
 import com.futureworkshops.mobileworkflow.domain.PluginFactory
+import com.futureworkshops.mobileworkflow.model.plugin.PluginInformation
 import com.futureworkshops.mobileworkflow.plugin.charts.pie.NetworkPieChartPlugin
 import com.futureworkshops.mobileworkflow.plugin.charts.pie.PieChartPlugin
 
@@ -9,4 +10,9 @@ class ChartsPluginFactory : PluginFactory(
         PieChartPlugin(),
         NetworkPieChartPlugin()
     )
-)
+) {
+    override fun getInformation(): PluginInformation = PluginInformation(
+        name = BuildConfig.LIBRARY_PACKAGE_NAME,
+        version = BuildConfig.PLUGIN_VERSION
+    )
+}
