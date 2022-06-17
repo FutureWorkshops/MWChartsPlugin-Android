1d7e7ff17b234abe8a2763df5610d7e94ade34a0
jmcarcedo <josem@futureworkshops.com>
2022-06-17 16:20:46 +0200

    Implement new models and refactor item provider

---
+++ charts_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/charts/view/ItemProvider.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2022 FutureWorkshops. All rights reserved.
+ */
+
+package com.futureworkshops.mobileworkflow.plugin.charts.view
+
+import com.futureworkshops.mobileworkflow.data.network.task.URLIAsyncTask
+import com.futureworkshops.mobileworkflow.domain.service.ServiceContainer
+import com.futureworkshops.mobileworkflow.plugin.charts.step.DashboardBaseChartItem
+import com.futureworkshops.mobileworkflow.plugin.charts.step.DashboardChartItem
+import com.futureworkshops.mobileworkflow.plugin.charts.step.DashboardNetworkChartItem
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+sealed class ItemProvider {
+
+    abstract fun onItemsReady(
+        provider: (List<DashboardBaseChartItem>) -> Unit,
+        onLoading: () -> Unit,
+        onError: () -> Unit
+    )
+
+    open fun retry() {}
+
+    class SyncItemsProvider(private val items: List<DashboardChartItem>) : ItemProvider() {
+        override fun onItemsReady(
+            provider: (List<DashboardBaseChartItem>) -> Unit,
+            onLoading: () -> Unit,
+            onError: () -> Unit
+        ) {
+            val formalisedItems = items.map {
+                DashboardBaseChartItem(
+                    id = it.id,
+                    listItemId = it.listItemId,
+                    chartType = it.chartType,
+                    title = it.title,
+                    subtitle = it.subtitle,
+                    footer = it.footer,
+                    values = it.statisticText?.let(::listOf) ?: it.chartValues?.split(",")
+                    ?: emptyList(),
+                    colors = it.chartColors?.split(","),
+                    colorsDark = it.chartColorsDark?.split(",")
+                )
+            }
+            provider(formalisedItems)
+        }
+    }
+
+    class AsyncItemsProvider(
+        private val serviceContainer: ServiceContainer,
+        private val task: URLIAsyncTask<Nothing, List<DashboardNetworkChartItem>>
+    ) : ItemProvider() {
+        private lateinit var success: (List<DashboardBaseChartItem>) -> Unit
+        private lateinit var loading: () -> Unit
+        private lateinit var error: () -> Unit
+
+        override fun retry() {
+            super.retry()
+            load()
+        }
+
+        override fun onItemsReady(
+            provider: (List<DashboardBaseChartItem>) -> Unit,
+            onLoading: () -> Unit,
+            onError: () -> Unit
+        ) {
+            success = provider
+            loading = onLoading
+            error = onError
+            load()
+        }
+
+        private fun load() {
+            loading()
+            serviceContainer.performSingle<URLIAsyncTask<Nothing, List<DashboardNetworkChartItem>>, List<DashboardNetworkChartItem>>(
+                task
+            ).subscribeOn(Schedulers.io())
+                .map { items ->
+                    items.map {
+                        DashboardBaseChartItem(
+                            id = it.id,
+                            listItemId = it.id,
+                            chartType = it.chartType,
+                            title = it.title,
+                            subtitle = it.subtitle,
+                            footer = it.footer,
+                            values = it.statisticText?.let(::listOf) ?: it.chartValues
+                            ?: emptyList(),
+                            colors = it.chartColors,
+                            colorsDark = it.chartColorsDark
+                        )
+                    }
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { success(it) },
+                    { error ->
+                        error()
+                        error.printStackTrace()
+                    }
+                )
+        }
+    }
+}
